2d67a70c308562d190048b21e189b274ee122ba1
2026sergio <mirandasegio550@gmail.com>
2026-05-07 09:16:23 -0600

    feat: :sparkles: function, destructuring, placing the country with its name
    function, destructuring, placing the country with its name

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
 fetch("https://disease.sh/v3/covid-19/countries")
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+
+        const contenedor = document.getElementById("idContenedorResultados");
+        data.forEach(pais => {
+
+            contenedor.innerHTML += `
+                    <h2>${pais.country}</h2>
+                    <img src="${pais.countryInfo.flag}" width="50px" alt="Bandera">`;
+    });
+})
+async function mostrarPaises(){
+    let paises = JSON.paises(localStorage.getItem("paises"))
+    if (paises){
+        console.log(paises)
+    }
+}
