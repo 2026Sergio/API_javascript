db8c7996809eee0203c4181ecfe39436f520fd86
Sergio Ricardo Ajù Miranda <mirandasegio550@gmail.com>
2026-05-08 01:55:15 +0000

    feat: :sparkles: Add a function to search for a country using a search engine.
    Add a function to search for a country using a search engine.

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
+const contenedor = document.getElementById("idContenedorResultados");
+const nodoBusqueda = document.getElementById("idEntradaBusqueda");
+
+let listaPaisesBase = [];
 
 fetch("https://disease.sh/v3/covid-19/countries")
     .then(response => response.json())
     .then(data => {
 
-        const contenedor = document.getElementById("idContenedorResultados");
-        data.forEach(pais => {
+        listaPaisesBase = data; 
+        localStorage.setItem("paises", JSON.stringify(data));
+        
+        pintarEnPantalla(data);
+    })
+    .catch(error => console.error("Error:", error));
 
-            contenedor.innerHTML += `
-                    <h2>${pais.country}</h2>
-                    <img src="${pais.countryInfo.flag}" width="50px" alt="Bandera">`;
-    });
-})
-async function mostrarPaises(){
-    let paises = JSON.paises(localStorage.getItem("paises"))
-    if (paises){
-        console.log(paises)
+nodoBusqueda.oninput = () => {
+
+    const textoUsuario = nodoBusqueda.value.toLowerCase();
+
+    const filtrados = listaPaisesBase.filter(pais => 
+        pais.country.toLowerCase().includes(textoUsuario)
+    );
+
+    pintarEnPantalla(filtrados);
+};
+function pintarEnPantalla(lista) {
+    let htmlFinal = ""; 
+
+    if (lista.length === 0) {
+        contenedor.innerHTML = "<p>No se encontró ningún país con ese nombre.</p>";
+        return;
     }
+
+    lista.forEach(pais => {
+        htmlFinal += `
+            <article class="tarjeta">
+                <h2>${pais.country}</h2>
+                <img src="${pais.countryInfo.flag}" width="50px" alt="Bandera">
+            </article>`;
+    });
+
+    contenedor.innerHTML = htmlFinal;
 }
